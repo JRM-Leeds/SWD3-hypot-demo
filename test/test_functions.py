b77f4cec61c15d3c1039a2d0206c3283545dafa6
James Middleton <prejmi@leeds.ac.uk>
2023-11-21 14:05:44 +0000

    add test functions

---
+++ test/test_functions.py
@@ -0,0 +1,18 @@
+# test addition
+def test_addition_int():
+    assert addition(4,7) == 4
+
+
+# test squared
+def test_squared_odd():
+    assert squared (-2) == 4
+
+
+# test squared root
+def test_squroot():
+    assert sqroot(9) == 3
+
+
+# test hypot
+def test_hypot():
+    assert hypot(3,5)=4
